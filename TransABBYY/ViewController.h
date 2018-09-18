d18a857ac3b3da6fb660fc1e1c15ab112c504d74
meirinder <s.kulizhnikov@gmail.com>
2018-09-18 13:13:44 +0700

    Initial Commit

---
+++ TransABBYY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransABBYY
+//
+//  Created by Savely on 18.09.2018.
+//  Copyright © 2018 Savely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TransABBYY/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TransABBYY
+//
+//  Created by Savely on 18.09.2018.
+//  Copyright © 2018 Savely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
